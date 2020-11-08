030ee80dbdfbe08ee31e00015108fe770bb76ea1
Michał Adamczyk <hello@codeman.it>
2020-11-08 10:56:47 +0100

    add movie Tile component

---
+++ src/common/Tile/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import "./style.css";
+
+export const Tile = ({ title, url, description }) => {
+
+    const MAX_TEXT_LENGTH = 25;
+
+    const cutText = (description) => {
+        if (description.length > MAX_TEXT_LENGTH) {
+            return `${description.slice(0, MAX_TEXT_LENGTH)}...`;
+        }
+        return description;
+    };
+
+    return (
+        <div className="tileWrapper">
+            <h2>{title}</h2>
+            <ReactPlayer
+                className="tileWrapper__player"
+                url={url}
+                width="400px"
+                height="400px"
+                controls
+                light
+            />
+
+            <div className="tileWrapper__descriptionBox">
+                <span>{cutText(description)}</span>
+                <button className="tileWrapper__button">Show more</button>
+            </div>
+        </div>
+    );
+};
