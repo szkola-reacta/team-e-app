339fc42f46c3d5e5f6bda5db90902d0c0cf58441
val089 <kamilszerlag@gmail.com>
2020-11-14 16:04:32 +0100

    refactor styles

--- src/common/Header/HeaderComponents/Logo.js
+++ src/common/Header/HeaderComponents/Logo.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Logo({ image, alt }) {
     return(
-        <div className="App__header__logo__container">
-            <img className="App__header__logo__img" src={image} alt={alt} />
-            <h1 className="App__header__logo__text">MovieApp</h1>
+        <div className="header__logo__container">
+            <img className="header__logo__img" src={image} alt={alt} />
+            <h1 className="header__logo__text">MovieApp</h1>
         </div>
     );
 }
--- src/common/Header/HeaderComponents/Menu.js
+++ src/common/Header/HeaderComponents/Menu.js
@@ -8,12 +8,12 @@ function Menu({ opened }) {
     };
 
     return(
-        <nav className="App__header__nav">
-            <ul className="App__header__nav__links" style={styles}>
-                <li><NavLink exact to="/" className="App__header__nav__link" activeClassName="App__header__nav__link__active">Home</NavLink></li>
-                <li><NavLink exact to="/videolist" className="App__header__nav__link" activeClassName="App__header__nav__link__active">Video List</NavLink></li>
-                <li><NavLink exact to="/about" className="App__header__nav__link" activeClassName="App__header__nav__link__active">About</NavLink></li>
-                <li><NavLink exact to="/addmovie" className="App__header__nav__link" activeClassName="App__header__nav__link__active">Add Movie</NavLink></li>
+        <nav className="header__nav">
+            <ul className="header__links__list" style={styles}>
+                <li><NavLink exact to="/" className="header__link" activeClassName="header__link__active">Home</NavLink></li>
+                <li><NavLink exact to="/videolist" className="header__link" activeClassName="header__link__active">Video List</NavLink></li>
+                <li><NavLink exact to="/about" className="header__link" activeClassName="header__link__active">About</NavLink></li>
+                <li><NavLink exact to="/addmovie" className="header__link" activeClassName="header__link__active">Add Movie</NavLink></li>
             </ul>
         </nav>
     );
